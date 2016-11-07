33f424731b11f50c1a3c3c16c8ccef43d0ab48b0
omartnc <omer_tunc_04@hotmail.com>
2016-11-07 23:20:54 +0300

    added Profile and update login,register

--- TeknikServisYonetimSistemi/Teknik.MVC/Controllers/AccountController.cs
+++ TeknikServisYonetimSistemi/Teknik.MVC/Controllers/AccountController.cs
@@ -1,8 +1,15 @@
-﻿using System;
+﻿using Microsoft.AspNet.Identity;
+using Microsoft.Owin.Security;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Threading.Tasks;
 using System.Web;
 using System.Web.Mvc;
+using Teknik.BLL.Account;
+using Teknik.BLL.Settings;
+using Teknik.Entity.IdentityModels;
+using Teknik.Entity.ViewModels;
 
 namespace Teknik.MVC.Controllers
 {
@@ -13,9 +20,109 @@ namespace Teknik.MVC.Controllers
         {
             return View();
         }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> Register(LoginAndRegisterViewModel Model)
+        {
+            if (!ModelState.IsValid)
+                return View(Model);
+            var userManager = MembershipTools.NewUserManager();
+            var checkUser = userManager.FindByName(Model.Register.Name);
+            if (checkUser != null)
+            {
+                ModelState.AddModelError(string.Empty, "Bu kullanıcı zaten kayıtlı !");
+                return View(Model);
+            }
+            var aktivasyonKodu = Guid.NewGuid().ToString().Replace("-", "");
+            var user = new ApplicationUser()
+            {
+                UserName = Model.Register.Username,
+                Name = Model.Register.Name,
+                Surname = Model.Register.Surname,
+                PhoneNumber = Model.Register.PhoneNumber,
+                Email = Model.Register.Email,
+                ActivationCode = aktivasyonKodu
+            };
+            var sonuc = userManager.Create(user, Model.Register.Password);
+            if (sonuc.Succeeded)
+            {
+                if (userManager.Users.ToList().Count == 1)
+                {
+                    userManager.AddToRole(user.Id, "Admin");
+                    await SiteSettings.SendMail(new MailModel()
+                    {
+                        To = user.Email,
+                        Subject = "Hoşgeldiniz",
+                        Message = $"Merhaba {user.UserName}, </br> Sisteme Admin rolünde kayıt oldunuz. <br/><a href='http://localhost:14320/Account/Profile'>Profil Sayfanız</a>"
+                    });
+                }
+                else
+                {
+                    userManager.AddToRole(user.Id, "Passive");
+                    await SiteSettings.SendMail(new MailModel()
+                    {
+                        To = user.Email,
+                        Subject = "Hoşgeldiniz",
+                        Message = $"Merhaba {user.UserName}, </br> Sisteme başarı ile kayıt oldunuz. <br/> Hesabınızı aktifleştirmek için <a href='http://localhost:14320/Account/Activation?code={aktivasyonKodu}'>Aktivasyon Kodu</a>"
+                    });
+                }
+                return RedirectToAction("Index", "Home");
+            }
+            else
+            {
+                ModelState.AddModelError(string.Empty, "Kullanıcı kayıt işleminde hata oluştu!");
+                return View(Model);
+            }
+        }
         public ActionResult Login()
         {
             return View();
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> Login(LoginAndRegisterViewModel model)
+        {
+            if (!ModelState.IsValid)
+                return View(model);
+            var userManager = MembershipTools.NewUserManager();
+            var user = await userManager.FindAsync(model.Login.KullaniciAdi, model.Login.Sifre);
+            if (user == null)
+            {
+                ModelState.AddModelError(string.Empty, "Böyle bir kullanıcı bulunamadı.");
+                return View(model);
+            }
+            var authManager = HttpContext.GetOwinContext().Authentication;
+            var userIdentity = await userManager.CreateIdentityAsync(user, DefaultAuthenticationTypes.ApplicationCookie);
+            authManager.SignIn(new AuthenticationProperties()
+            {
+                IsPersistent = model.Login.HatirlansinMi
+            }, userIdentity);
+            return RedirectToAction("Index", "Home");
+        }
+        [Authorize]
+        public ActionResult Logout()
+        {
+            var authManager = HttpContext.GetOwinContext().Authentication;
+            authManager.SignOut();
+            return RedirectToAction("Index", "Home");
+        }
+        [Authorize]
+        public ActionResult Profile()
+        {
+            var userManager = MembershipTools.NewUserManager();
+            var id = HttpContext.User.Identity.GetUserId();
+            var user = userManager.FindById(id);
+
+            ProfileViewModel model = new ProfileViewModel()
+            {
+                Email = user.Email,
+                Name = user.Name,
+                Surname = user.Surname,
+                Username = user.UserName,
+                PhoneNumber = user.PhoneNumber
+            };
+
+            return View(model);
         }
     }
 }
--- TeknikServisYonetimSistemi/Teknik.MVC/Global.asax.cs
+++ TeknikServisYonetimSistemi/Teknik.MVC/Global.asax.cs
@@ -1,9 +1,12 @@
-﻿using System;
+﻿using Microsoft.AspNet.Identity;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using System.Web.Routing;
+using Teknik.BLL.Account;
+using Teknik.Entity.IdentityModels;
 
 namespace Teknik.MVC
 {
@@ -13,6 +16,39 @@ namespace Teknik.MVC
         {
             AreaRegistration.RegisterAllAreas();
             RouteConfig.RegisterRoutes(RouteTable.Routes);
+            var roleManager = MembershipTools.NewRoleManager();
+            if (!roleManager.RoleExists("Admin"))
+            {
+                roleManager.Create(new ApplicationRole()
+                {
+                    Name = "Admin",
+                    Description = "Site Yöneticisi"
+                });
+            }
+            if (!roleManager.RoleExists("User"))
+            {
+                roleManager.Create(new ApplicationRole()
+                {
+                    Name = "User",
+                    Description = "Standart kayıtlı üye"
+                });
+            }
+            if (!roleManager.RoleExists("Banned"))
+            {
+                roleManager.Create(new ApplicationRole()
+                {
+                    Name = "Banned",
+                    Description = "Yasaklı Üye"
+                });
+            }
+            if (!roleManager.RoleExists("Passive"))
+            {
+                roleManager.Create(new ApplicationRole()
+                {
+                    Name = "Passive",
+                    Description = "Mail Aktivasyonu Gerekli"
+                });
+            }
         }
     }
 }
