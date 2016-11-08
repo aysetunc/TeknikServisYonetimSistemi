3a457c2187b67858a4bc740f7b1832dfba502095
omartnc <omer_tunc_04@hotmail.com>
2016-11-08 15:22:14 +0200

    added admin setting controller

---
+++ TeknikServisYonetimSistemi/Teknik.Entity/ViewModels/SettingsViewModel.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Teknik.Entity.ViewModels
+{
+    public class SettingsViewModel
+    {
+        public List<PcMarkaViewModel> PcMarkalari = new List<PcMarkaViewModel>();
+        public List<PcModelViewModel> PcModelleri = new List<PcModelViewModel>();
+        public List<KullaniciViewModel> Kullanicilar = new List<KullaniciViewModel>();
+        public List<YönetimYetkiViewModel> Yetkiler = new List<YönetimYetkiViewModel>();
+        public PcModelViewModel PcModeli { get; set; }
+        public PcMarkaViewModel PcMarkasi { get; set; }
+    }
+    public class PcMarkaViewModel
+    {
+        public int ID { get; set; }
+        [StringLength(70)]
+        [Required]
+        [Display(Name = "Marka Adı")]
+        public string MarkaAdi { get; set; }
+    }
+    public class PcModelViewModel
+    {
+        public int ID { get; set; }
+        [Required]
+        [Display(Name = "Model Adı")]
+        public string ModelAdi { get; set; }
+    }
+    public class KullaniciViewModel
+    {
+        public string ID { get; set; }
+        [Display(Name = "E-mail")]
+        public string Email { get; set; }
+        [Display(Name = "Kullanıcı Adı")]
+        public string KullaniciAdi { get; set; }
+        [Display(Name = "Kullanıcı Rolü")]
+        public string Rol { get; set; }
+    }
+    public class YönetimYetkiViewModel
+    {
+        public string  Yetki { get; set; }
+    }
+}
---
+++ TeknikServisYonetimSistemi/Teknik.MVC/Controllers/AdminController.cs
@@ -0,0 +1,63 @@
+﻿using Microsoft.AspNet.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Mvc;
+using Teknik.BLL.Account;
+using Teknik.BLL.Repository;
+using Teknik.Entity.IdentityModels;
+using Teknik.Entity.ViewModels;
+
+namespace Teknik.MVC.Controllers
+{
+    [Authorize(Roles = "Admin")]
+    public class AdminController : Controller
+    {
+        // GET: Admin
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult Settings()
+        {
+            var userManager = MembershipTools.NewUserManager();
+            List<ApplicationUser> kullanicilar = userManager.Users.ToList();
+
+            var model = new SettingsViewModel()
+            {
+                PcMarkalari = new PcMarkaRepo().GetAll().Select(x => new PcMarkaViewModel()
+                {
+                    ID = x.ID,
+                    MarkaAdi = x.MarkaAdi
+                }).ToList(),
+                PcModelleri = new PcModelRepo().GetAll().Select(x => new PcModelViewModel()
+                {
+                    ID = x.ID,
+                    ModelAdi = x.ModelAdi
+                }).ToList(),
+                Kullanicilar = kullanicilar.Select(x => new KullaniciViewModel()
+                {
+                    ID = x.Id,
+                    Email = x.Email,
+                    KullaniciAdi = x.UserName,
+                    Rol = MembershipTools.NewRoleManager().FindById(x.Roles.FirstOrDefault().RoleId).Name
+                }).ToList(),
+                 Yetkiler=MembershipTools.NewRoleManager().Roles.ToList().Select(x=> new YönetimYetkiViewModel()
+                 {
+                      Yetki=x.Name
+                 }).ToList()
+            };
+            return View(model);
+        }
+
+        #region PartialViews
+        public PartialViewResult AdminMenu()
+        {
+            return PartialView("_adminmenuPartial");
+        }
+        #endregion
+    }
+
+}
